265b79c7736dccec07865300fe8e3008d8ea1e16
wufangfang0614 <wufangfang@neotel.com.cn>
2017-08-07 23:07:47 +0800

    6-1  -  6-3

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
+import Layout from './components/Layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
+let router = new VueRouter({
+    mode:'history',
+    routes:[
+        {
+          path:'/',
+          component: IndexPage
+        }
+    ]
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: '<Layout/>',
+  components: { Layout }
 })
